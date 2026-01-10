e1de45702400bcc76d084d325090bf01f05516e3
Josh Mayer <josh.25.mayer@gmail.com>
2026-01-10 16:42:14 -0500

    Initial Commit

---
+++ fade/fadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fadeApp.swift
+//  fade
+//
+//  Created by Josh Mayer on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct fadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fadeTests/fadeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fadeTests.swift
+//  fadeTests
+//
+//  Created by Josh Mayer on 1/10/26.
+//
+
+import Testing
+@testable import fade
+
+struct fadeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
